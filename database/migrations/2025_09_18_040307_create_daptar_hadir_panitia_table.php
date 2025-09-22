9240796def65551090044a05e5d57c16af59de82
Suryadi <if22.suryadi@mhs.ubpkarawang.ac.id>
2025-09-22 08:25:37 +0700

    peburbahan absen oleh siapa

--- database/migrations/2025_09_18_040307_create_daptar_hadir_panitia_table.php
+++ database/migrations/2025_09_18_040307_create_daptar_hadir_panitia_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('qr_code_datang')->nullable();
             $table->string('qr_code_pulang')->nullable();
             $table->foreignId('penerimaan_panitia_id')->constrained('penerimaan_panitia')->onDelete('cascade');
-            $table->foreignId('create_by')->constrained('users')->onDelete('cascade')->nullable();
+            $table->foreignId('absen_by')->constrained('users')->onDelete('cascade')->nullable();
             $table->foreignId('update_by')->constrained('users')->onDelete('cascade')->nullable();
             $table->timestamps();
         });
