451a4f7d328d34a39c48262d09375cc73ad807c4
Muhammad Jibrail Natadilaga-FS10575740 <74393318+Jibrail0398@users.noreply.github.com>
2025-11-02 22:59:14 +0700

    Update and rename dokumentasi.php to Dokumentasi.php

--- app/Models/Dokumentasi.php
+++ app/Models/Dokumentasi.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class dokumentasi extends Model
+class Dokumentasi extends Model
 {
     use HasFactory;
 
